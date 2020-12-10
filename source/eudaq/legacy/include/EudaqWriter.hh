fb4118398b28aad9741499a84c9b10b3bee67833
YI LIU <yi.liu@desy.de>
2020-12-10 22:43:54 +0000

    remove unused files

--- source/eudaq/legacy/include/EudaqWriter.hh
+++
@@ -1,8 +0,0 @@
-#include "JadeCore.hh"
-#include "eudaq/Producer.hh"
-
-class EudaqWriter: public JadeWriter {
-public:
-  using JadeWriter::JadeWriter;
-  virtual void SetProducerCallback(eudaq::Producer *producer) = 0;
-};
